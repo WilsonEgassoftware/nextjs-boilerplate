5a7e51fdd827b8cb75013abef4fe17306096a0ee
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-11-28 20:12:45 -0500

    Add NextAuth configuration with Auth0 provider

---
+++ auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import Auth0 from "next-auth/providers/auth0";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Auth0({
+      name: "Auth0",
+      clientId: process.env.AUTH_AUTH0_ID,
+      clientSecret: process.env.AUTH_AUTH0_SECRET,
+      issuer: `https://${process.env.AUTH_AUTH0_DOMAIN}`,
+    }),
+  ],
+});
